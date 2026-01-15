e9f2234ccc028b5be9f974c173863f55f31d60a4
Mahmudjon <mrfx404@gmail.com>
2026-01-15 17:57:18 +0500

    templatlar togrlandi

--- Kutubxona/Kutubxona/settings.py
+++ Kutubxona/Kutubxona/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Kutubxona.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('templates'))],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
