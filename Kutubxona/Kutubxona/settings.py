3865b35a931e09386b7cd1b2a1960dbe67038aeb
Mahmudjon <mrfx404@gmail.com>
2026-01-24 16:04:08 +0500

    Kod xatolari topildi

--- Kutubxona/Kutubxona/settings.py
+++ Kutubxona/Kutubxona/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6e97ll4&1_yhj0d53!j*_n!epfigi8)o(^2cwakcj8!4e)@r^9'
+# SECRET_KEY = 'django-insecure-6e97ll4&1_yhj0d53!j*_n!epfigi8)o(^2cwakcj8!4e)@r^9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = config('DEBUG', default=False, cast=bool)
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS =config('ALLOWED_HOSTS')
 
 # Application definition
 
