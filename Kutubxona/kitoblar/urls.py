a0757cdb709546ebb8a0ab3b17c5b202cf0c75c2
Jahongir <toshtemirovjahongir24@gmail.com>
2026-03-06 16:04:50 +0500

    yangilanish

--- Kutubxona/kitoblar/urls.py
+++ Kutubxona/kitoblar/urls.py
@@ -1,15 +1,28 @@
-from django.urls import path
+from django.urls import path, include
+from . import views  # Barcha views'larni olish uchun
 from .views import (
     HomePageView, 
     AboutPageView, 
     FavoritPageView, 
     kitblist,
-    yurakcha)
+    yurakcha,
+    register, 
+    sozlamalar
+)
 
 urlpatterns = [ 
-    path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
     path('favorit/', FavoritPageView.as_view(), name='favorit'),
     path('kitoblar/', kitblist, name='kitoblar'),
-    path('<int:yurak_id>/yurakcha', yurakcha, name="yurakcha")   #Bu yurakcha funksiyasi ishga tushiradi.  
+    path('yurakcha/<int:yurak_id>/', yurakcha, name="yurakcha"), 
+    path('sozlamalar/', sozlamalar, name='sozlamalar'), 
+    
+    # Muhim qism: HTMLda 'kabinet' va 'logout' ishlatilgan bo'lsa, 
+    # views.py faylingizda shu nomli funksiyalar bo'lishi shart.
+    path('kabinet/', views.kabinet_view, name='kabinet'), 
+    path('logout/', views.logout_view, name='logout'),
+    
+    path('accounts/', include('django.contrib.auth.urls')), 
+    path('register/', register, name='register'),
 ]
--- Kutubxona/kitoblar/views.py
+++ Kutubxona/kitoblar/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404 # Hammasi shu yerda bo'lishi shart
 from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
-from django.views.generic import ListView
+from django.views.generic import TemplateView, ListView
 from django.urls import reverse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Kitob
 from decouple import config
 
@@ -24,10 +23,17 @@ def sevimli(request):
 def kitoblar(request):
     return render(request, 'kitoblar.html')
 
+def sozlamalar(request):
+    return render(request, 'sozlamalar.html')
 
+def kabinet_view(request):
+   
+    return render(request, 'kabinet.html')
 
+def logout_view(request):
+    
+    return redirect('home')
 
-# Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
 
@@ -38,39 +44,44 @@ class FavoritPageView(ListView):
     model = Kitob
     template_name = "favorit.html"    
 
-
-    # bu funksiya is_favorite ustunning True 
-    # qiymaga ega bo'lgan qatorni qaytaradi.
     def get_queryset(self):
         return Kitob.objects.filter(is_favorite=True)
-    
-
-# class KitobListView(ListView):
-#     model = Kitob
-#     template_name = "kitob.html"
 
 def kitblist(request):
     kitoblar = Kitob.objects.all()
-    
     return render(request, 'kitob.html', {'kitoblar': kitoblar})
-# Bu yurakchani ustiga bosilganda htmldan 
-# keladigan post ni olib yurakchani to'ldiradi yoki to'ldirmaydi
+
 def yurakcha(request, yurak_id):
     try:
+        # get_object_or_404 endi ishlaydi
         tanlangan_yurak = get_object_or_404(
             Kitob,
-            pk=request.POST["yurakcha"]
-            )
-        target_id = request.POST['yurakcha']
-
-    except Kitob.DoesNotExist:
+            pk=request.POST.get("yurakcha") # .get ishlatish xavfsizroq
+        )
+    except (Kitob.DoesNotExist, KeyError):
         raise Http404("Yurakcha bilan nimadir xatolik ketti...")
     else:
-        if tanlangan_yurak.is_favorite:
-            tanlangan_yurak.is_favorite = False
-        else:
-            tanlangan_yurak.is_favorite = True
-
+        tanlangan_yurak.is_favorite = not tanlangan_yurak.is_favorite
         tanlangan_yurak.save()
-
         return HttpResponseRedirect(reverse("kitoblar"))
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pas = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Bu foydalanuvchi nomi allaqachon band!')
+            return render(request, 'register.html')
+
+        if not pas or len(pas) < 4:
+            messages.error(request, 'Parol juda qisqa! Kamida 4 ta belgi bo‘lishi kerak.')
+            return render(request, 'register.html')
+
+        user = User.objects.create_user(username=username, password=pas)
+        user.save()
+
+        messages.success(request, 'Muvaffaqiyatli ro‘yxatdan o‘tdingiz!')
+        return redirect('sozlamalar')
+
+    return render(request, 'register.html')
